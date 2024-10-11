86e56926a29e9bbd70760200a8a82a3aba2d686a
AyoubDafir <youkdafir@gmail.com>
2024-10-11 16:58:46 +0200

    Primers exercicis de JavaScript

---
+++ operadors.js
@@ -0,0 +1,120 @@
+// //Operadors
+//     //Operadors aritmètics
+
+//     //4.1.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+// let a = 25;
+// let b = 15 + a;
+// let c = b * 2;
+// console.log(a, b, c);
+
+// //4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+// let a = 10;
+// let b = 20;
+// let c = 5;
+// a = a + 3;
+// b = b + 4 - a;
+// c = a + b + c;
+// a = a + c;
+// b = 4;
+// c = c + 3 - b + 2;
+// console.log(a,b,c);
+
+// //4.3.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+// //Escriu-los en un paper i desprès executa el codi. Et donen el mateix?
+// let a = 5;
+// let b = 18;
+// let c = 15;
+// let d = 22;
+// a = a + 10;
+// b = b + 5 - c;
+// c = c + 4 + b;
+// d = d + b + a;
+// console.log(a,b,c,d);
+// a = a + 1;
+// b = b + c;
+// c = b + c;
+// d = b + b;
+// console.log(a,b,c,d);
+
+// //4.4.- Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer  sobre paper i desprès prova a l’ordinador. 
+// let a = 10 
+// let b = 20 
+// let c = 5 
+// a = a + 3 // Fixat en aquesta instrucció.  
+// // Quin valor tenia la variable a abans  
+// // d’executar-se la instrucció? 
+// // I desprès? 
+// console.log(a); 
+// b = b + 4 - a 
+// console.log(b); 
+// c = a + b + c 
+// console.log(c); 
+// a = a + c 
+// console.log(a); 
+// b = 4 
+// console.log(b); 
+// c = c + 3 - b + 2 
+// console.log(c);
+
+// //4.5. - Quins valors s’escriuran a la consola si executem aquest programa? Fes-ho primer  sobre paper i desprès prova a l’ordinador. 
+// let a = 5; 
+// let b = 18; 
+// let c = 15; 
+// let d = 22; 
+// a = a + 10; 
+// b = b + 5 - c; 
+// c = c + 4 + b; 
+// d = d + b + a; 
+// console.log(a,b,c,d); 
+// a = a + 1; 
+// b = b + c; 
+// c = b + c; 
+// d = b + b; 
+// console.log(a,b,c,d); 
+
+// //4.6. - Quins valors s’escriuran a la consola si executem aquest programa? 
+// let a = 10; 
+// let b = 5; 
+// let c = a / b; 
+// let d = a * b; 
+// let e = a % 3; 
+// console.log (c,d,e); 
+
+// //4.7.- Quins valors s’escriuran a la consola si executem aquest programa? 
+// let a = 10; 
+// let b = 5; 
+// let c = a * b * 3; 
+// let d = a / 3; 
+// console.log(c,d);
+
+        //Operadors unaris 
+
+// //5.1.- Què s’escriurà a la consola si executem aquest codi? 
+// let a = 5; 
+// a++; 
+// console.log(a); 
+// let b = a++; 
+// console.log(a,b); 
+
+// //5.2.- Què s’escriurà a la consola si executem aquest codi? 
+// let a = 5; 
+// ++a; 
+// console.log(a); 
+// let b = ++a; 
+// console.log(a,b); 
+
+// //5.3.- Observa aquestes operacions, prova a executar-les i explica el perquè dels resultats  obtinguts. 
+// let a = 5; 
+// let b = a++; 
+// let c = a--; 
+// console.log(a,b,c); 
+// a = 5; 
+// b = ++a; 
+// c = --a; 
+// console.log(a,b,c); 
+// a = 5; 
+// let d = 25 + a++; 
+// a = 5; 
+// let e = 25 + ++a; 
+// console.log(d,e);
+
