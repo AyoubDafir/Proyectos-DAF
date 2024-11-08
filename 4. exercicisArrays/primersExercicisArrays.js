3d0b0eb77759d77201f836c24f4f94bf58af8809
AyoubDafir <youkdafir@gmail.com>
2024-11-08 15:25:42 +0100

    Canvis

---
+++ 4. exercicisArrays/primersExercicisArrays.js
@@ -0,0 +1,30 @@
+                                                //Exercicis de taules
+                                        //Introducció a les taules
+                                  
+//     //21.1.- Fes un programa que faci el següent:
+// // ● Defineix una variable de tipus taula, buida.
+// let arr2=[];
+
+// // ● Afegeixi el valor “Hola” a la posició 0.
+// arr2[0]="Hola";
+
+// // ● Afegeixi el valor “Adeu” a la posició 1.
+// arr2[1]="Adeu";
+
+// // ● Escrigui a la consola el contingut de la taula.
+// console.log(arr2);
+
+// // 21.2.- Al programa anterior, afegeix les instruccions necessàries (després del console.log()) per a fer el següent:
+// arr2[2]="Nom";
+// console.log(arr2);
+// arr2[1]=",";
+// console.log(arr2);
+
+//21.3.- Fes un programa que faci el següent:
+let taula=[23,45,98,73];
+let suma=taula[0]+taula[2];
+taula[6]=suma;
+console.log(taula);
+
+//21.4.- Fes un programa que, donat un número (que demanarem per pantalla fent servir prompt()) emmagatzemi a una taula el doble i el triple d’aquest número. Un cop fet, escriu la taula a la consola.
+    
