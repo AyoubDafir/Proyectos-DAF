3577f0369c016cf048a37be87dd71ee6a6da827e
AyoubDafir <youkdafir@gmail.com>
2024-12-05 18:43:55 +0100

    new

--- 0. classe_teoria/classe_JV_abançat/classe_JV.js
+++ 0. classe_teoria/classe_JV_abançat/classe_JV.js
@@ -1,3 +1,11 @@
+        //Altres maneres de definir funcions
+//     //Expressió de funció anònima
+// const quadrat = function (number) {
+//     return number * number;
+//     };
+//     let x = quadrat(4); // Assignem 16 a x
+
+    //Expressió de funció amb nom
 let num =parseInt(prompt("diguem un número"));
 const factorial = function fac(n) {
     return n < 2 ? 1 : n * fac(n - 1);
