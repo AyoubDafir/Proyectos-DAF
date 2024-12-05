e6a454d2f6fd20baf4c95ef3ecc713e1f277bb6f
AyoubDafir <youkdafir@gmail.com>
2024-12-05 18:55:23 +0100

    new content

--- 0. classe_teoria/classe_JV_abançat/classe_JV.js
+++ 0. classe_teoria/classe_JV_abançat/classe_JV.js
@@ -5,10 +5,26 @@
 //     };
 //     let x = quadrat(4); // Assignem 16 a x
 
-    //Expressió de funció amb nom
-let num =parseInt(prompt("diguem un número"));
-const factorial = function fac(n) {
-    return n < 2 ? 1 : n * fac(n - 1);
-    };
-    console.log(factorial(num));
-    
+//     //Expressió de funció amb nom
+// let num =parseInt(prompt("diguem un número"));
+// const factorial = function fac(n) {
+//     return n < 2 ? 1 : n * fac(n - 1);
+//     };
+//     console.log(factorial(num));
+
+//     //Mètodes per treballar amb arrays
+// //Funció map(), modifica l'array a partir de la funció passada com a paràmetre
+// let taula = [1,5,10,15];
+// const fun = function (num) {
+// return num*4;
+// }
+// let novaTaula = taula.map(fun);
+// console.log(novaTaula);
+
+// //Funció filter(), Filtra el contingut de l'array, creant un nou array només amb els elements seleccionats.
+// let taula = [1,5,10,15,23,56,92,34,67];
+// const fun = function (num) {
+// return num % 2 === 0;
+// }
+// let novaTaula = taula.filter(fun);
+// console.log(novaTaula);
